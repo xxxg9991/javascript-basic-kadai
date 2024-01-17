434319b9b0207533e9722c9891afcbe233f3ea0d
xxxg9991 <153571560+xxxg9991@users.noreply.github.com>
2024-01-17 21:01:31 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,26 @@
+// 四則計算
+console.log(68)/(1.7*1.7);
+
+//定数の宣言・値の代入
+const height = prompt("身長をメートル単位で入力してください");
+const weight = prompt("体重をキログラム単位で入力してください");
+
+// コンソールへの出力
+console.log(height);
+console.log(weight);
+
+//変数の宣言
+/*let Height;
+let Weight;
+
+// 値の代入
+Height = "1.7"
+Weight = "68"*/
+
+//変数の宣言・値の代入 
+let bmi = weight / height / height;
+console.log(bmi);
+
+// コンソールへの出力
+/*console.log(Height);
+console.log(Weight);*/
